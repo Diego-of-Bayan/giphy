6768c70e4b100a4bbb313a25f623e84279440c86
Diego Ramil <dbramil811@gmail.com>
2019-05-23 23:59:53 +0000

    message

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
 $("#search-button").click(function(){
-  
-  
-  
+  let search = $("#search-term").val();
+$.ajax({
+  url: "https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=8Jc9gh1J97JiQtCrZsDsEYJxoj1HlcxH",
+method: "GET",
+  success: function(response) {
+    console.log("ASD");
+    $("#az1").empty();
+    $("#az1").append("<img src='" + response.data[0].images.original.url + "'>");
+  },
+});
 });
 
